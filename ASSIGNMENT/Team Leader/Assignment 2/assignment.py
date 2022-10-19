d72cf2524fed4653926f10f2ba12abbca9c0477f
RUDHRANM <116088529+RUDHRANM@users.noreply.github.com>
2022-10-19 22:34:54 +0530

    Update assignment.py

--- ASSIGNMENT/Team Leader/Assignment 2/assignment.py
+++ ASSIGNMENT/Team Leader/Assignment 2/assignment.py
@@ -1,1 +1,10 @@
-import 
+import random
+while(True):
+  a=random.ramdiant(10,99)
+  b==random.randiant(10,99)
+  if(a>35 and b>60):
+    print("High temperature and humidity of:",a,b,"%","Alaram is on")
+  elif(a<35 and b<60):
+        print("Normal temperature and humidity of:",a,b,"%","Alaram is off")
+  break
+
